106dfa09e6626868369fe2e31c489fd906c1f84b
DoBayy <159333365+DoBayy@users.noreply.github.com>
2024-03-20 23:12:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+let intro = document.querySelector('.intro');
+let logo = document.querySelector('.logo-header');
+let logoSpan = document.querySelectorAll('.logo');
+
+window.addEventListener('DOMContentLoaded', ()=>{
+    setTimeout(()=>{
+
+        logoSpan.forEach((span, idx)=>{
+            setTimeout(()=>{
+                span.classList.add('active');
+            },(idx+1) * 400) 
+        });
+
+        setTimeout(()=>{
+            logoSpan.forEach((span, idx)=>{
+                setTimeout(()=>{
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                },(idx + 1) * 50)
+            })
+        },2000);
+
+        setTimeout(()=>{
+            intro.style.top = '-100vh';
+        },2300);
+
+    })
+})
 window.addEventListener('scroll', function() {
     var navbar = document.getElementById('black');
     if (window.scrollY > 100) { // Adjust the scroll threshold as needed
